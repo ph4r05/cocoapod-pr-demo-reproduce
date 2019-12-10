7a3dfca3cbbbc1c4cd6df4ca85a81990a038ea96
Dusan Klinec <dusan.klinec@gmail.com>
2019-12-10 09:33:15 +0100

    Initial Commit

---
+++ cocoapod-pr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cocoapod-pr
+//
+//  Created by Dusan Klinec on 10/12/2019.
+//  Copyright © 2019 Dusan Klinec. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
